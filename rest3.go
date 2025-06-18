74f9bb4c4fd5dc9eb1e3f9946ce67d3b768d9009
Luís Arthur <luisarthurofficial@gmail.com>
2025-06-18 09:01:47 -0300

    docs: Update rest3
    added conditional %3 and %5 return PinPan

--- rest3.go
+++ rest3.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	for i := 1; i <= 100; i++ {
-		if i%3 == 0 {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("Pin Pan")
+		} else if i%3 == 0 {
 			fmt.Println("Pin")
 		} else if i%5 == 0 {
 			fmt.Println("Pan")
